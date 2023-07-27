3f0d26b652d2eec65895cd935c64d23679b7ad39
DeckofKard <qww9363@gmail.com>
2023-07-27 14:15:48 +0900

    get lck match by json

--- src/contracts/Vote.sol
+++ src/contracts/Vote.sol
@@ -4,42 +4,47 @@ import './SetTeam.sol';
 // Vote.sol
 
 contract Vote is SetTeam {
+    // user가 베팅하는 함수
     function vote(uint256 _amount, string memory _teamName) public returns(bool) {
-        // Check if the user have voetd
+        // 투표는 한 번만 가능하기에 이미 했었는지 확인
         require(!userData[msg.sender].voted, "You have already voted!");
-        // Needs more balance than the amount you want to invest
+        // 자신이 가진 돈 이하만 베팅할 수 있어야함
         require(balanceOf(msg.sender) >= _amount, "Not enough tokens!");
 
-        // get the index of the team which you want to vote 
+        // 베팅하고 싶은 팀의 index 받아옴
         uint index = getTeamIndexByTeamName(_teamName);
         require(index != INVALID_INDEX, "Invalid team name");
 
-        // transfers tokens to the team, voteBalance 
+        // 박은 돈 -> 팀의 베팅 묶인 돈으로 옮김
         userData[msg.sender].balance -= _amount;
         teamWeight[index] += _amount;
+        // 나중에 gameEnd()가 실행됬을 때, 돈을 되돌려 받을 때 비율 계산을 위해서 박은 돈의 양 저장
         userData[msg.sender].voteBalance += _amount; 
 
-        // user have voted
+        // 베팅 했으니 겜 끝날 때까지 베팅 못하도록 제한걸어둠
         userData[msg.sender].voted = true;
+        // 어떤 팀에 베팅했었는지 저장
         userData[msg.sender].teamIndex = index;
         
         return true;
     }
 
-    // update winnerTeamIndex(in SetTeam.sol)
+    // 어떤 팀이 이겼었는지 설정함
+    // SetTeam.sol에 있는 winnerTeamIndex값을 초기화함.
     function gameEnd(string memory _teamNameWin, string memory _teamNameLose) public onlyOwner returns(uint) {
-        // get the winner team index
+        // 이긴 팀, 진 팀의 index 받아와서
         uint _winnerTeamIndex = getTeamIndexByTeamName(_teamNameWin);
         uint _loserTeamIndex = getTeamIndexByTeamName(_teamNameLose);
         require(_winnerTeamIndex != INVALID_INDEX, "Invalid winner team name");
         require(_loserTeamIndex != INVALID_INDEX, "Invalid loser team name");
 
         winnerTeamIndex = _winnerTeamIndex;
-        // Loser Team weight -> Winer Team weight
-        winnerTeamPureBalance = teamWeight[_winnerTeamIndex];      // 순수 winner team 총 베팅 금액(비율 계산용)
+        // 순수 winner team 총 베팅 금액(비율 계산용)
+        winnerTeamPureBalance = teamWeight[_winnerTeamIndex];      
+        // 진 팀에 묶인 돈을 모두 이긴 팀에 옮김
         teamWeight[_winnerTeamIndex] += teamWeight[_loserTeamIndex];
 
-        // 총 묶인 돈 : bettingTotalBalance
+        // 총 묶인 돈 : bettingTotalBalance(계산 용으로 따로 빼둠)
         bettingTotalBalance = teamWeight[_winnerTeamIndex];
         teamWeight[_loserTeamIndex] = 0;
 
@@ -47,18 +52,20 @@ contract Vote is SetTeam {
     }
 
 
-    // 진 팀 베팅한 사람은 voted 여전히 true로 남아남남
+    // 진 팀 베팅한 사람 : 나중에 투표 또 할 수 있게 투표권 다시 부여
+    // 이긴 팀 베팅한 사람 : 돈 주고, 투표권 다시 부여
     function returnBettingResult() public {
         require(winnerTeamIndex != INVALID_INDEX, "winner team index not set");
 
         if (getUserVotedTeamIndex(msg.sender) == winnerTeamIndex) {
+            // 자신이 넣은 돈에 비례해서
             uint amountToReturn = (voteBalanceOf(msg.sender) / winnerTeamPureBalance) * bettingTotalBalance;
-
+            // teamWeight[winner]에서 차감되면서 user에게 나누어줌
             userData[msg.sender].balance += amountToReturn;
             teamWeight[winnerTeamIndex] -= amountToReturn;
         }
         // user betting 관련 data 초기화
-         userData[msg.sender].teamIndex = INVALID_INDEX;
+        userData[msg.sender].teamIndex = INVALID_INDEX;
         userData[msg.sender].voteBalance = 0;
         userData[msg.sender].voted = false;
     }
@@ -70,5 +77,6 @@ contract Vote is SetTeam {
         require(winnerTeamIndex != INVALID_INDEX, "winner team index not set");
         totalSupply += teamWeight[winnerTeamIndex];
         teamWeight[winnerTeamIndex] = 0;
+        winnerTeamIndex = INVALID_INDEX;
     }
 }
